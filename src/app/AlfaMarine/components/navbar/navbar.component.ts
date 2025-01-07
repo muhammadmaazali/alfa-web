2f273a9fa6b0b2135480e530060983a97e7b019e
maaz.ali <maaz.ali@hive-worx.com>
2025-01-07 13:27:27 +0500

    Fixes

--- src/app/AlfaMarine/components/navbar/navbar.component.ts
+++ src/app/AlfaMarine/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FooterComponent, MainMenuComponent
+  imports: [FooterComponent, MainMenuComponent,RouterModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
